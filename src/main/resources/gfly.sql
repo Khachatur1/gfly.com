7430c71cda68381f4f2b07b9d780b919c0ad3a56
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-03-27 12:33:17 +0400

    gfly

--- src/main/resources/gfly.sql
+++ src/main/resources/gfly.sql
@@ -1,19 +1,18 @@
 /*
-SQLyog Ultimate v11.5 (64 bit)
-MySQL - 5.7.21-log : Database - gfly
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.7.21 : Database - gfly
 *********************************************************************
 */
 
-
 /*!40101 SET NAMES utf8 */;
 
-/*!40101 S ET SQL_MODE=''*/;
+/*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`gfly` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`gfly` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `gfly`;
 
@@ -24,12 +23,21 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `pic_url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `category` */
 
-insert  into `category`(`id`,`name`) values (1,'biplane'),(2,'fly wing'),(3,'hidroplane'),(4,'multirotor'),(5,'3d'),(6,'planer'),(7,'triner'),(8,'copy');
+insert  into `category`(`id`,`name`,`pic_url`) values 
+(1,'3D','3d.jpg'),
+(2,'BIPLANE','biplane.jpg'),
+(3,'COPI','copi.jpeg'),
+(4,'FLYWING','flywing.jpg'),
+(5,'HIDROPLANE','hidroplane.gif'),
+(6,'MULTIROTOR','multirotor.jpg'),
+(7,'PLANER','planer.png'),
+(8,'TRINER','triner.jpg');
 
 /*Table structure for table `image` */
 
@@ -56,13 +64,33 @@ CREATE TABLE `product` (
   `item_number` varchar(255) NOT NULL,
   `category_id` int(11) NOT NULL,
   `description` text NOT NULL,
-  `video_url` varchar(255) DEFAULT NULL,
+  `pic_url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `product_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `product` */
+
+insert  into `product`(`id`,`name`,`item_number`,`category_id`,`description`,`pic_url`) values 
+(1,'ebab','adbfb',1,'sbb','1.png'),
+(2,'roirjo','oapbo',2,'opdb','1.png'),
+(3,'oepbtob','opbog',3,'opgbgo','1.png');
+
+/*Table structure for table `video` */
+
+DROP TABLE IF EXISTS `video`;
+
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `video_url` varchar(255) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `video_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `video` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
