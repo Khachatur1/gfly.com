6736ea1848979e955cd6de2a9895cec4890b1a9f
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-03-30 00:22:59 +0400

    Add Post.java

---
+++ src/main/java/am/gfly/model/Post.java
@@ -0,0 +1,28 @@
+package am.gfly.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String title;
+    @Column
+    private String description;
+    @Column(name = "pic_url")
+    private String picUrl;
+    @ManyToOne
+    private User user;
+}
