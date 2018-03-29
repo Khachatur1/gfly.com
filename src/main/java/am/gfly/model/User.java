1c27c570b189c5b0bc91081cd1b7620f3b831048
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-03-30 00:20:46 +0400

    Add User.java

---
+++ src/main/java/am/gfly/model/User.java
@@ -0,0 +1,28 @@
+package am.gfly.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+}
