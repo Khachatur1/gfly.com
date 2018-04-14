46a2601b4973b96beca24e824ee21f3da92accfd
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-04-14 13:18:01 +0400

    add EmbeddedServletContainerCustomizer @Bean

--- src/main/java/am/gfly/GflyApplication.java
+++ src/main/java/am/gfly/GflyApplication.java
@@ -1,13 +1,15 @@
 package am.gfly;
 
-import am.gfly.model.User;
-import am.gfly.model.UserType;
+
 import am.gfly.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,9 +19,10 @@ import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
 @EnableWebMvc
-public class GflyApplication extends WebMvcConfigurerAdapter{
-@Autowired
+public class GflyApplication extends WebMvcConfigurerAdapter {
+    @Autowired
     UserRepository userRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(GflyApplication.class, args);
     }
@@ -36,21 +39,25 @@ public class GflyApplication extends WebMvcConfigurerAdapter{
 
     }
 
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
 
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/undefined");
+                ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+                container.addErrorPages(error401Page, error404Page, error500Page);
+            }
+        };
+    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
 
-//    @Override
-//    public void run(String... strings) throws Exception {
-//        User user = new User();
-//        user.setName("admin");
-//        user.setSurname("admin");
-//        user.setEmail("admin@mail.com");
-//        user.setPassword("admin");
-//        user.setType(UserType.ADMIN);
-//        userRepository.save(user);
-//    }
+
 }
