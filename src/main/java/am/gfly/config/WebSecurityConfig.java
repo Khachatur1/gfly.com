b332691f5f6ba9dfc9633965bc47d32a13dbe348
Karo Darbinyan <darbinyan.karo@gmail.com>
2018-04-17 01:18:02 +0400

    update project

--- src/main/java/am/gfly/config/WebSecurityConfig.java
+++ src/main/java/am/gfly/config/WebSecurityConfig.java
@@ -1,17 +1,13 @@
 package am.gfly.config;
 
 
-import am.gfly.handler.CustomAuthenticationFailureHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .defaultSuccessUrl("/loginSuccess")
-                .failureHandler(new CustomAuthenticationFailureHandler())
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login")
